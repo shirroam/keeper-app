f95e449c83a90504c8ca0bdf3842db94b5b48b1c
Shirley <smbiayuk@gmail.com>
2024-10-17 17:32:46 +0200

    Completed section 317, notes can be added and deleted

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, {
+        title: note.title,
+        content: note.content,
+      }];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note, index) => {
+        return index !== id;
+      })
+    })
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote} />
+      <div>
+      {notes.map((note, index) => (
+            <Note
+              key={index}
+              id={index}
+              title={note.title}
+              content={note.content}
+              onDelete={deleteNote}
+            />
+          ))}
+      </div>
       <Footer />
     </div>
   );
